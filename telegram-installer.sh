d7810400ff0220d3f9de2a362f2425e69ebef1ec
silvernode <silvernode@gmail.com>
2015-09-21 20:00:35 -0700

    Added symlink, added uninstall, renamed LICENSE

--- telegram-installer.sh
+++ telegram-installer.sh
@@ -19,6 +19,7 @@
 URL="https://tdesktop.com/linux"
 FILE="tsetup.tar.xz"
 OUTDIR="${HOME}/.telegram/bin"
+SYMDIR="/usr/bin"
 
 #check and create directories
 checkDir(){
@@ -30,7 +31,7 @@ checkDir(){
       return false
     fi
   else
-    return true
+    return 0
   fi
 }
 
@@ -39,43 +40,87 @@ askOpen(){
 
   echo -n "Do you want to run telegram now?[y/n]"
   read runNow
-  
+
   if [ "${runNow}" = "y" ];then
     ${OUTDIR}/Telegram/Telegram
   else
-    exit 0;
+    return 1
+  fi
+}
+
+createSym(){
+  echo -n "Do you want to create a symlink to ${SYMDIR}?[y/n](requires root): "
+  read symchoice
+
+  if [ ${symchoice} = "n" ];then
+    return 1
+  else
+    sudo ln -s -v ${OUTDIR}/Telegram/Telegram ${SYMDIR}/telegram
+  fi
+}
+chkInstall(){
+
+  if [ -d ~/.telegram ];then
+    echo
+    echo -n "Telegram is already installed, would you like to uninstall it?[y/n]: "
+    read choice
+
+    if [ "${choice}" = "y" ];then
+      echo
+      echo "Removing symlink from ${SYNDIR}..."
+      sudo rm /usr/bin/telegram
+      echo
+      echo "Removing ${OUTDIR}..."
+      echo
+      rm -r -v ~/.telegram
+      if [ ! -d ~/.telegram ];then
+        echo
+        echo "Telegram is uninstalled"
+        exit 0;
+      else
+        echo
+        echo "Telegram is still installed, please remove it manually"
+      fi
+    else
+      echo
+      echo "Skipping removal"
+      exit 0;
+    fi
+  else
+    echo
+    echo "Telegram is not installed"
   fi
 }
 
 #Pull everything together
 main(){
+  chkInstall
   checkDir
   if [ ! checkDir ];then
     echo "Looks like something went wrong creating the directory"
-    
+
   else
-    
+
     if [ ! -f /tmp/${FILE} ];then
       wget -O /tmp/${FILE} ${URL}
       tar xvf /tmp/${FILE} -C ${OUTDIR}
       echo
       askOpen
+      createSym
     elif [ -f /tmp/${FILE} ];then
       tar xvf /tmp/${FILE} -C ${OUTDIR}
       echo
+      createSym
       askOpen
-      
     else
       echo "Looks like the file does not exist"
       echo "Check your internet connection and system permissions"
       return false
     fi
   fi
-  
-    
+
+
 }
 
+
 main
-
-
-
